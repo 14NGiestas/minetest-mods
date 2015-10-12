0f580eba9534397c5f3f597d02959a2f2e4d40fa
Ian Giestas <iangiestaspauli@gmail.com>
2015-10-12 15:48:19 -0300

    deleted:    README.md 	deleted:    pythonic/em.py~ 	deleted:    pythonic/init.lua~ 	deleted:    pythonic/teste.py

--- README.md
+++
@@ -1,2 +0,0 @@
-My mods
-# minetest-mods
--- pythonic/teste.py
+++
@@ -1,8 +0,0 @@
-import pyttsx2,sys
-engine = pyttsx2.init()
-try:
-	Query = sys.argv[1].split(':')[1].replace('_',' ')
-except:
-	Query = sys.argv[1]
-engine.say(Query)
-engine.runAndWait()
